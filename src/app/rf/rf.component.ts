c53a2aab9a8839f4fd3a872815723a496e9f0a9a
rajni farswan <rajnifarswan14@gmail.com>
2020-02-03 02:08:34 +0530

    jjj

--- src/app/rf/rf.component.ts
+++ src/app/rf/rf.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-rf',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rf.component.scss']
 })
 export class RfComponent implements OnInit {
-
-  constructor() { }
+formName : FormGroup;
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit() {
+    // this.formName = new FormGroup({
+    //   fullName : new FormControl(),
+    //   email : new FormControl(),
+    //   skills : new FormGroup({
+    //         skillName: new FormControl(),
+    //         experienceYears: new FormControl(),
+    //         proficiency: new FormControl()
+
+    //   })
+    // })
+
+    this.formName = this.fb.group({
+      fullName : [''],
+      email : [''],
+      skills : this.fb.group({
+        skillName : [''],
+        experienceYears :[''],
+        proficiency:['biginner']
+      })
+    })
+  }
+  loadDataClick(): void{
+this.formName.patchValue({
+  fullName :'rajni farswan',
+  email: 'rajni@gmail.com',
+  skills:{
+    skillName :'angular',
+    experienceYears:'4',
+    proficiency : 'intermediater'
+
   }
 
+})
+  } 
+  onSubmit(): void{
+    console.log(this.formName.touched);
+    console.log(this.formName.value);
+    console.log(this.formName.controls.fullName.touched);
+    console.log(this.formName.get('fullName').value);
+  }
 }
