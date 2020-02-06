005cce3c91f6641ae27723fb6d3e79552fdf132f
rajni farswan <rajnifarswan14@gmail.com>
2020-02-07 01:20:17 +0530

    loadbutton

--- src/app/rf/rf.component.ts
+++ src/app/rf/rf.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, NgModule } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
+
 @Component({
   selector: 'app-rf',
   templateUrl: './rf.component.html',
@@ -8,6 +9,7 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 })
 export class RfComponent implements OnInit {
 formName : FormGroup;
+fullNameLength = 0;
   constructor( private fb: FormBuilder) { }
 
   ngOnInit() {
@@ -31,19 +33,41 @@ formName : FormGroup;
         proficiency:['biginner']
       })
     });
+    // this.formName.get('fullName').valueChanges.subscribe((value: any) => {
+    //   this.fullNameLength = value.length;
+    // });
+    this.formName.valueChanges.subscribe((value: any) => {
+      console.log(JSON.stringify(value));
+    });
   }
+logKeyObjectPairs(group: FormGroup): void{
+Object.keys(group.controls).forEach((key) => {
+const abstractControl = group.get(key);
+if( abstractControl instanceof FormGroup){     // to check nested form group
 
+  //abstractControl.disable();     // disable all nested form controls
+  this.logKeyObjectPairs(abstractControl);
+}
+else{
+ // console.log('key : ' + key + 'value : ' + abstractControl.value);
+ //abstractControl.disable();     // disable all form controls
+  abstractControl.markAsDirty();  // dirty form control 
+}
+});
+
+}
 
   loadDataClick(): void{
-this.formName.patchValue({
-  fullName :'rajni farswan',
-  email: 'rajni@gmail.com',
-  skills:{
-    skillName :'angular',
-    experienceYears:'4',
-    proficiency : 'intermediater'
-  }
-})
+// this.formName.patchValue({
+//   fullName :'rajni farswan',
+//   email: 'rajni@gmail.com',
+//   skills:{
+//     skillName :'angular',
+//     experienceYears:'4',
+//     proficiency : 'intermediater'
+//   }
+// })
+this.logKeyObjectPairs(this.formName);
   };
   onSubmit(): void{
     console.log(this.formName.touched);
